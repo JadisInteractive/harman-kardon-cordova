53ff4403eec1476eb2447257fb95d765671cfc2e
Troy Nunnally <troynunnally@brainrainsolutions.com>
2015-08-14 14:05:10 -0400

    add alert view

--- src/ios/HKAudio.m
+++ src/ios/HKAudio.m
@@ -9,12 +9,32 @@
     NSString* name = [[command arguments] objectAtIndex:0];
     NSString* msg = [NSString stringWithFormat: @"Hello, %@", name];
 
+
+    //HKWControlHandler *hkwController = [[HKWControlHandler alloc] init];
+
+    //hkwController = [HKWControlHandler sharedInstance];
+
+    //if (!hkwController.isInitialized ) {
+
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Initializing"
+                                                    message:@"If this dialog appears, please check if any other HK WirelessHD App is running on the phone and kill it. Or, your phone is not in a Wifi network."
+                                                   delegate:self
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil];
+    [alert show];
+
+
+    //}
+
     CDVPluginResult* result = [CDVPluginResult
+
                                resultWithStatus:CDVCommandStatus_OK
                                messageAsString:msg];
 
     [self.commandDelegate sendPluginResult:result callbackId:callbackId];
 }
+
+
 
 
 @end
